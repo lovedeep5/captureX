b750973de77ce357690c57dabaf7c90b3d4d5dee
lovedeep5 <38611031+lovedeep5@users.noreply.github.com>
2024-04-02 02:12:43 +0530

    Deploy to vercel (#4)
    * Updating the live link for app
    
    * Added videos to library
    
    * eslint uninstalled
    
    * Feature/redux toolkit integration (#3)
    
    * Updating the live link for app (#1)
    
    * Develop (#2)
    
    * Updating the live link for app
    
    * Added videos to library
    
    * eslint uninstalled
    
    * Redux tool kit added
    
    * Recording feature added - In progress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+import StoreProvider from "./(redux)/StoreProvider";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] })
@@ -18,7 +20,9 @@ export default function RootLayout ({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <StoreProvider>{children}</StoreProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
