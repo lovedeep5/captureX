810d051bc9cbe4f206ca277e4f9afefb40c13e5e
lovedeep5 <38611031+lovedeep5@users.noreply.github.com>
2024-04-06 13:39:04 +0530

    Recording Panel Created (#5)
    * recording panel integration in progress
    
    * recording panel completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,26 +4,31 @@ import { Inter } from "next/font/google";
 import StoreProvider from "./(redux)/StoreProvider";
 
 import "./globals.css";
+import RecordButton from "@/components/RecordButton";
+import RecordingPanel from "@/components/RecordingPanel";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Capture",
   description: "Record video and share easily",
-}
+};
 
-export default function RootLayout ({
-  children
+export default function RootLayout({
+  children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <StoreProvider>{children}</StoreProvider>
+          <StoreProvider>
+            <RecordingPanel />
+            {children}
+          </StoreProvider>
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
